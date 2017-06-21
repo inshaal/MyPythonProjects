cfdf522165e5ff39347fcb7cbd4f14e061728c99
Inshaal Uddin <inshaal@users.noreply.github.com>
2017-06-21 16:10:13 +0530

    Add files via upload

---
+++ School/recursive_factorial.py
@@ -0,0 +1,8 @@
+#To find factorial of a no. using recursive method loop
+def fact(n):
+    if (n==0):
+        return 1
+    else:
+        return n*fact(n-1)
+n=input("Enter any no. ")
+print fact(n)
